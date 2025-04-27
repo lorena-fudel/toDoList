6b2eedc484586e9d0866dcb8488fef4c0715391c
lorena <fmrlorena@gmail.com>
2025-04-27 22:19:09 +0100

    imprimir listas por pantalla

---
+++ todolist/src/main/java/com/todolist/todolist/controllers/ObtenerListasControllers.java
@@ -0,0 +1,11 @@
+package com.todolist.todolist.controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name="obtenerListas", urlPatterns={"/obtenerListas"})
+public class ObtenerListas extends HttpServlet {
+
+    
+
+}
